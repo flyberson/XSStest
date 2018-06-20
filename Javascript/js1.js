c1e08792114670d44e7a6d411d7dd639c5edb51c
root <root@instance-1.c.owd-martin.internal>
2018-06-20 00:03:02 +0000

    idk

--- Javascript/js1.js
+++ Javascript/js1.js
@@ -15,6 +15,7 @@ function getpoke(){
 
     //$( "#body" ).append( userinputtext );
     document.write(userinputtext);
+	location.reload();
     //document.getElementById("body").appendChild(document.createTextNode(userinputtext));
     //<script>window.location.replace("https://www.google.dk/")</script>
     //http://35.204.168.251:3000/hook.js
